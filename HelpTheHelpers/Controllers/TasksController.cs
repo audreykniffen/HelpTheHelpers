81179c93e688355e3b0afc5999da6838c33f6e4d
audreykniffen <audrey.kniffen@gmail.com>
2021-04-17 15:19:05 -0500

    V.2 after package problems, with Add Function

---
+++ HelpTheHelpers/Controllers/TasksController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelpTheHelpers.Controllers
+{
+    public class TasksController : Controller
+    {
+        // GET: /<controller>/
+       // [HttpGet]
+
+        public IActionResult Index()
+        {
+            List<string> Tasks = new List<string>();
+            Tasks.Add("Laundry");
+            Tasks.Add("Get Medicine");
+            Tasks.Add("Fence Repair");
+
+            ViewBag.tasks = Tasks;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            // Any additional method code here
+
+            return View();
+        }
+   
+    }
+}
