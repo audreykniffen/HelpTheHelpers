f00f0a9b10f4f85e35a42637c3415d48842eba2e
audreykniffen <audrey.kniffen@gmail.com>
2021-04-17 20:06:08 -0500

    Delete Funciton before MySQL integration

--- HelpTheHelpers/Controllers/TasksController.cs
+++ HelpTheHelpers/Controllers/TasksController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using HelpTheHelpers.Data;
+using HelpTheHelpers.Models;
+
 
 namespace HelpTheHelpers.Controllers
 {
     public class TasksController : Controller
     {
-        static private List<Task> Tasks = new List<Task>();
-
       
         public IActionResult Index()
         {
@@ -17,22 +18,40 @@ namespace HelpTheHelpers.Controllers
 
             return View();
         }
-
-        [HttpGet]
         public IActionResult Add()
         {
-            // Any additional method code here
-
             return View();
         }
+            // Any additional method code here
 
-        [HttpPost]
-        [Route("/Tasks/Add")]
-        public IActionResult NewTask(string name, string desc)
+ 
+    [HttpPost]
+    [Route("Tasks/Add")]
+    public IActionResult NewTask (string name, string desc)
+    {
+        TaskData.Add(new Task(name, desc));
+
+
+        return Redirect("/Tasks");
+    }
+
+    public IActionResult Delete()
+    {
+        ViewBag.tasks = TaskData.GetAll();
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int[] eventIds)
+    {
+        foreach (int eventId in eventIds)
         {
-            Tasks.Add(new Task(name, desc));
-
-            return Redirect("/Tasks");
+           TaskData.Remove(eventId);
         }
+
+        return Redirect("/Events");
+    }
+
     }
 }
